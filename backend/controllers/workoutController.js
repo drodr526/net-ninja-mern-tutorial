0ea944165dd8e230b5e0d5259acfe187aa4d383d
Daniel Rodriguez <drodr526@fiu.edu>
2024-10-20 14:14:57 -0400

    create basic submit workout form , fixed error handling bug

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -32,7 +32,7 @@ const createWorkout = async (req, res) => {
         const workout = await Workout.create({ title, load, reps })
         res.status(200).json(workout);
     } catch (error) {
-        response.status(400).json({ error: error.message });
+        res.status(400).json({ error: error.message });
     }
 }
 
