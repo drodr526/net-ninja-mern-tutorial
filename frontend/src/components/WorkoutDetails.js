613fe924cf7e5c9e67d4e37781b39f46f69dff43
Daniel Rodriguez <drodr526@fiu.edu>
2024-10-20 16:32:38 -0400

    add formatDistanceToNow functionality

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,5 +1,7 @@
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
+//date fns
+import formatDistanceToNow from "date-fns/formatDistanceToNow"
 const WorkoutDetails = ({workout}) => {
 const {dispatch} = useWorkoutsContext()
 
@@ -19,7 +21,7 @@ const {dispatch} = useWorkoutsContext()
             <h4>{workout.title}</h4>
             <p><strong>Load (kg): </strong>{workout.load}</p>
             <p><strong>Reps: </strong>{workout.reps}</p>
-            <p>{workout.createdAt}</p>
+            <p>{formatDistanceToNow(new Date(workout.createdAt), {addSuffix: true})}</p> {/*super convenient library!! */}
             <span className="material-symbols-outlined" onClick={handleClick}>delete</span> {/* word inside the span changes icon */}
         </div>
     )
