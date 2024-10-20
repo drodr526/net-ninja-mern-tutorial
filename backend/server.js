27cda4c1772554bce9a91ac48cf72c049b2efeda
Daniel Rodriguez <drodr526@fiu.edu>
2024-10-19 22:07:32 -0400

    rename workout.js in routes folder to workoutsRouter

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 
 const express = require('express');
 const mongoose = require("mongoose");
-const workoutRoutes = require("./routes/workouts")
+const workoutRoutes = require("./routes/workoutsRouter")
 
 //create express app
 const app = express();
