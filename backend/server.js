c04b7c5f1adf15ff66c5de2e5f5278096efed057
Daniel Rodriguez <drodr526@fiu.edu>
2024-10-23 11:14:25 -0400

    can now sign up users

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ require("dotenv").config()
 
 const express = require('express');
 const mongoose = require("mongoose");
-const workoutRoutes = require("./routes/workoutsRouter")
+const workoutRoutes = require("./routes/workoutsRouter");
+const userRoutes = require("./routes/userRoutes.js");
 
 //create express app
 const app = express();
@@ -18,6 +19,7 @@ app.use((req, res, next) => {
 
 //routes
 app.use("/api/workouts", workoutRoutes);
+app.use("/api/user", userRoutes);
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
